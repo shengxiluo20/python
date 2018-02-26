22732d6aacf4762539a46cdc6976900c109aeec0
迟世超 <chi18765928341@163.com>
2018-02-26 20:40:39 +0800

    string

--- splinter/test_splinter.py
+++ splinter/test_splinter.py
@@ -3,6 +3,22 @@ from time import sleep
 
 
 browser = Browser("chrome")
-browser.visit("https://a.libfintech.com/login")
+browser.visit("https://www.jd.com/?cu=true")
 
-text = browser.find_by_text('用户名')
+
+browser.find_by_text("你好，请登录").click()
+
+browser.find_by_text("账户登录").click()
+
+browser.find_by_id("loginname").fill("11111111111")
+browser.find_by_id("nloginpwd").fill("111111")
+browser.find_by_id("loginsubmit").click()
+
+sleep(3)
+browser.visit("https://order.jd.com/center/list.action")
+sleep(3)
+browser.find_by_text("收货地址").last.click()
+sleep(3)
+browser.find_by_text("新增收货地址").click()
+
+

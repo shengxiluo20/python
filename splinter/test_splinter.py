d69279a59272baf55e1d766ba7209cedadb87797
迟世超 <chi18765928341@163.com>
2018-03-07 17:33:46 +0800

    增加手动输入验证码

--- splinter/test_splinter.py
+++ splinter/test_splinter.py
@@ -10,8 +10,15 @@ browser.find_by_text("你好，请登录").click()
 
 browser.find_by_text("账户登录").click()
 
-browser.find_by_id("loginname").fill("11111111111")
-browser.find_by_id("nloginpwd").fill("111111")
+browser.find_by_id("loginname").fill("1221")
+browser.find_by_id("nloginpwd").fill("1221")
+
+by_id = browser.find_by_id("JD_Verification1")
+
+if by_id:
+    name = input("请手动输入验证码.....")
+    browser.find_by_id("authcode").fill(name)
+
 browser.find_by_id("loginsubmit").click()
 
 sleep(3)

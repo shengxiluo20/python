27aaa58e0d68a5768fd5e44a7b3966fdf2ad3d8f
迟世超 <chi18765928341@163.com>
2018-03-09 10:28:45 +0800

    增加手动输入验证码

--- splinter/test_selenium.py
+++ splinter/test_selenium.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from time import sleep
 
 driver = webdriver.Chrome()
 # driver.maximize_window()
@@ -6,14 +7,18 @@ driver.get('https://www.jd.com/?cu=true')
 driver.find_element_by_class_name("link-login").click()
 driver.find_element_by_link_text("账户登录").click()
 
-driver.find_element_by_id("loginname").send_keys("123")
-driver.find_element_by_id("nloginpwd").send_keys("123")
+driver.find_element_by_id("loginname").send_keys("111")
+
+driver.find_element_by_id("nloginpwd").send_keys("111")
 
 by_id = driver.find_element_by_id("JD_Verification1")
-print(len(by_id))
-if len(by_id) == 1:
-    print(by_id)
-    name = input("请手动输入验证码.....")
-    driver.find_element_by_id("authcode").send_keys(name)
+name = input("请手动输入验证码.....")
+driver.find_element_by_id("authcode").send_keys(name)
 
 driver.find_element_by_id("loginsubmit").click()
+
+sleep(3)
+driver.get("https://order.jd.com/center/list.action")
+driver.find_element_by_link_text("收货地址").click()
+sleep(3)
+driver.find_element_by_link_text("新增收货地址").click()
